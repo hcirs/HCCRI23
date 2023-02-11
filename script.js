c049b3fae33dfdf1e81ce38d51c19f09a5597870
iysr <42057563+iysr@users.noreply.github.com>
2023-02-11 16:39:04 +0800

    new sidebar fully functional

--- script.js
+++ script.js
@@ -2,9 +2,16 @@ let prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
   let currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos || currentScrollPos < 50) {
-    document.getElementsByClassName("red-nav")[0].classList.remove("hide");
+    document.getElementsByClassName("red-nav-m")[0].classList.remove("hide");
   } else {
-    document.getElementsByClassName("red-nav")[0].classList.add("hide");
+    document.getElementsByClassName("red-nav-m")[0].classList.add("hide");
   }
   prevScrollpos = currentScrollPos;
 };
+
+document.getElementById('side-open').addEventListener('click',()=>{
+  document.getElementsByClassName('red-nav-s')[0].style.width = '250px'
+})
+document.getElementById('side-close').addEventListener('click',()=>{
+  document.getElementsByClassName('red-nav-s')[0].style.width = '0px'
+})
