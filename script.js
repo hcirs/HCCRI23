50595d0e4ba6b4cf32a25b7949f5b855eb84e5a4
Liu Weichu <weichufmsp@gmail.com>
2023-03-19 01:08:00 +0800

    update content

--- script.js
+++ script.js
@@ -9,9 +9,26 @@ window.onscroll = function () {
   prevScrollpos = currentScrollPos;
 };
 
-document.getElementById('side-open').addEventListener('click',()=>{
-  document.getElementsByClassName('red-nav-s')[0].style.width = '250px'
-})
-document.getElementById('side-close').addEventListener('click',()=>{
-  document.getElementsByClassName('red-nav-s')[0].style.width = '0px'
-})
+document.getElementById("side-open").addEventListener("click", () => {
+  document.getElementsByClassName("red-nav-s")[0].style.width = "250px";
+});
+document.getElementById("side-close").addEventListener("click", () => {
+  document.getElementsByClassName("red-nav-s")[0].style.width = "0px";
+});
+
+// faq menu
+const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute("aria-expanded");
+
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute("aria-expanded", "false");
+  }
+
+  if (itemToggle == "false") {
+    this.setAttribute("aria-expanded", "true");
+  }
+}
+
+items.forEach((item) => item.addEventListener("click", toggleAccordion));
