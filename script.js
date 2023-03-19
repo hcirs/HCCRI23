3c9861d7455273c9d77cd5dc3826117ad0188144
iysr <42057563+iysr@users.noreply.github.com>
2023-03-19 13:17:11 +0800

    typos+countdown+navbar styling

--- script.js
+++ script.js
@@ -10,11 +10,37 @@ window.onscroll = function () {
 };
 
 document.getElementById("side-open").addEventListener("click", () => {
-  document.getElementsByClassName("red-nav-s")[0].style.width = "250px";
+  document.getElementsByClassName("red-nav-s")[0].style.width = "280px";
 });
 document.getElementById("side-close").addEventListener("click", () => {
   document.getElementsByClassName("red-nav-s")[0].style.width = "0px";
 });
+
+// countdown
+
+const targetdate = new Date(2023,3,1,12,0,0,0); //set as 1 april 2023 12pm, change as nec
+const confdate = new Date(2023,5,19,0,0,0) //19 june 2023, 12am
+let now = Date.now()
+
+if(document.getElementById('hours')){
+  if(now<=targetdate){
+    diff=parseInt(targetdate-now)
+    document.getElementById('days').innerText=`${Math.floor(diff/1000/60/60/24)} Days`
+    diff-=Math.floor(diff/1000/60/60/24)*1000*60*60*24
+    document.getElementById('hours').innerText=`${Math.floor(diff/1000/60/60)} Hours`
+    diff-=Math.floor(diff/1000/60/60)*1000*60*60
+    document.getElementById('minutes').innerText=`${Math.floor(diff/1000/60)} Minutes`
+  }
+  else{
+    diff=confdate-now
+    document.getElementById('event').innerText='to the Conference!'
+    document.getElementById('days').innerText=`${Math.floor(diff/1000/60/60/24)} Days`
+    diff-=Math.floor(diff/1000/60/60/24)*1000*60*60*24
+    document.getElementById('hours').innerText=`${Math.floor(diff/1000/60/60)} Hours`
+    diff-=Math.floor(diff/1000/60/60)*1000*60*60
+    document.getElementById('minutes').innerText=`${Math.floor(diff/1000/60)} Minutes`
+  }
+}
 
 // faq menu
 const items = document.querySelectorAll(".accordion button");
